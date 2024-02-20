3c7d976e0e6755bb85768db1bedbc96b6107f491
Dasajton <david.sajitz.94@gmail.com>
2024-02-20 21:57:29 +0100

    Refactor CreateWorkout component to include WorkoutGenerator

--- src/components/CreateWorkout/CreateWorkout.tsx
+++ src/components/CreateWorkout/CreateWorkout.tsx
@@ -1,14 +1,23 @@
-import { WorkoutInput } from "../../components";
+import { useContext } from "react";
+import { AppContext } from "../../AppContext";
+import { WorkoutInput, WorkoutGenerator } from "../../components";
 
 export const CreateWorkout = () => {
+  const { inputComplete, setInputComplete } = useContext(AppContext);
+
   const handleInputContinue = (workoutName: string, workoutDays: number) => {
     console.log("Workout Name: ", workoutName);
     console.log("Workout Days: ", workoutDays);
+    setInputComplete(true);
   };
 
   return (
     <>
-      <WorkoutInput onContinue={handleInputContinue} />
+      {!inputComplete ? (
+        <WorkoutInput onContinue={handleInputContinue} />
+      ) : (
+        <WorkoutGenerator />
+      )}
     </>
   );
 };
