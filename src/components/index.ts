c6e0044e0e25dee975ed4622eb8c2d2e185c1ae6
Dasajton <david.sajitz.94@gmail.com>
2024-02-20 18:35:09 +0100

    Add WorkoutInput component to index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import { Navbar } from "./Navbar/Navbar";
 import { WorkoutGenerator } from "./CreateWorkout/WorkoutGenerator";
+import { WorkoutInput } from "./CreateWorkout/WorkoutInput";
 import { Button } from "./Button/Button";
 
-export { Navbar, WorkoutGenerator, Button };
+export { Navbar, WorkoutGenerator, WorkoutInput, Button };
