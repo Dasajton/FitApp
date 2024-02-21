e424a2a21aae2eadb93252060626da10b0d9b49e
Dasajton <david.sajitz.94@gmail.com>
2024-02-21 14:58:58 +0100

    Remove unnecessary import statement in index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,6 @@ import { Navbar } from "./Navbar/Navbar";
 import { WorkoutInput } from "./CreateWorkout/WorkoutInput";
 import { WorkoutGenerator } from "./CreateWorkout/WorkoutGenerator";
 import { CreateWorkout } from "./CreateWorkout/CreateWorkout";
-
 import { Button } from "./Button/Button";
 
 export { Navbar, Button, WorkoutInput, WorkoutGenerator, CreateWorkout };
